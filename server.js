ccc389c4f51d275c72d7d30aa96d752243b2402c
unknown <tzangmo021@gmail.com>
2025-09-09 21:43:51 +0530

    Updated server.js with new changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const app=express();
-const PORT=process.env.PORT || 8080;
+const port=process.env.PORT || 8080;
 app.get("/",(req,res)=>{
     res.send("hello world");
 });
